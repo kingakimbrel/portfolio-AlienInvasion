7f77011aad1d2760548300c6a95a7b180210b977
Kinga <kinga.kimbrel@gmail.com>
2016-10-21 18:44:04 -0500

    game over feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 from settings import Settings
 from game_models.ship import Ship
 from game_functions.core import *
+from game_models.game_stats import GameStats
 
 
 def run_game():
@@ -11,6 +12,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Store game statistics
+    stats = GameStats(ai_settings)
 
     # Ship
     ship = Ship(ai_settings, screen)
@@ -22,9 +26,12 @@ def run_game():
 
     while True:
         check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        update_bullets(bullets)
-        update_aliens(ai_settings, aliens)
+
+        if stats.game_active:
+            ship.update()
+            update_bullets(ai_settings, screen, ship, bullets, aliens)
+            update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
---
+++ game_models/game_stats.py
@@ -0,0 +1,12 @@
+class GameStats():
+    """Track statistick for Alien Invasion"""
+
+    def __init__(self, ai_settings):
+        """Initialiaze statistics"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        self.game_active = True
+
+    def reset_stats(self):
+        """Reset game stats"""
+        self.ships_left = self.ai_settings.ship_limit
